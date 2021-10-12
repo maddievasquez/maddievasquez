f9c1fc9052dc2252813de75164da7f78470bdec7
maddievasquez <90685629+maddievasquez@users.noreply.github.com>
2021-10-12 14:14:00 +0100

    Add files via upload

---
+++ oop-lesson-4-parking/Models/Carpark.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_lesson_4_parking.Models
+{
+    class CarPark
+    {
+    }
+}
---
+++ oop-lesson-4-parking/Models/CarparkCharge.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_lesson_4_parking.Models
+{
+    public class CarparkCharge
+    {
+        public CarparkCharge()
+        {
+            Console.WriteLine("Starting CarparkCharge Class");
+        }
+        public int minimumFee { get; set; }
+        public int minimumHours { get; set; }
+        public CarParkCharge(int _minimumFee = 2, int _minimumHours = 3)
+        {
+            minimumFee = _minimumFee;
+            minimumHours = _minimumHours;
+            Console.WriteLine("Starting CarparkCharge Class");
+        }
+
+        public override string ToString()
+        {
+            return "---------------------CarparkCharge";
+        }
+
+        public int CalculateCharge(int hoursParked)
+        {
+
+
+            return hoursParked * minimumFee;
+        }
+    }
+}
